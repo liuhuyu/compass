f1f44a7960f1fdfe06cd3eff205af9de615be393
liuhuyu <735077491@qq.com>
2013-01-16 23:44:21 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Compass::Application.config.secret_token = 'be67fc34cbcc01e976a11af594cf48c8b5e375b84ce3d6e634d91b4e887600d50d03aecc0329507486aa8d33f90e3200465f686660daac460603988324f60294'
